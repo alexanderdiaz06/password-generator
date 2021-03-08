a85a86b9ef22ce3c5f6860a884e171170d4db485
Alexander <alexand6@uci.edu>
2021-03-07 21:07:33 -0800

    addedprompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = window.prompt("How long would you like your password to be? (Minimum of 8 characters and maximum of 128)")
+  console.log (length)
+
+  var lower = window.confirm("Would you like any lowercase letters to be in your password?")
+  console.log (lower)
+
+  var upper = window.confirm("Would you like any uppercase letters to be in your password?")
+  console.log (upper)
+
+  var numbers = window.confirm("Would you like any numbers to be in your password?")
+  console.log (numbers)
+
+  var symbols = window.confirm("Would you like any symbols to be in your password?")
+  console.log (symbols)
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
